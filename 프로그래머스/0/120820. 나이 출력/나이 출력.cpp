63a923882204afa7715adb84b74a7a503659da45
Jak-Jang <jjgk24211@gmail.com>
2025-05-26 11:29:59 +0900

    [level 0] Title: 나이 출력, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120820. 나이 출력/나이 출력.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int age) 
+{
+    int answer = 0;
+    answer = 2023 - age; //2022년 기준 한국나이
+    
+    return answer;
+}
