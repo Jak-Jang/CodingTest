44d70f1ff9a5262073f52f261bcb83d675dbb979
JakJang <jjgk24211@gmail.com>
2025-06-11 09:16:35 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 0.15 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.cpp
@@ -0,0 +1,17 @@
+#include <string> // str.substr(idx, range)
+using namespace std;
+
+int solution(string t, string p) 
+{
+    int answer = 0;
+
+    for(int i = 0; i < t.size() - p.size() + 1; i++)
+    {
+        if(t.substr(i, p.size()) <= p)
+        {
+            answer++;
+        }
+    }
+        
+    return answer;
+}
