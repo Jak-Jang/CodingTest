a8fe7292559bce9c38daee43ab6a76958fa45dab
JakJang <jjgk24211@gmail.com>
2025-06-17 09:55:25 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 19.28 ms, Memory: 5.23 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.cpp
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.cpp
@@ -1,18 +1,25 @@
 #include <string>
 #include <vector>
-#include <map>
 
 using namespace std;
 
-vector<int> solution(string s)
+vector<int> solution(string s) 
 {
-    map<char, int> mp;
     vector<int> answer;
-    for (int i = 0; i < s.size(); ++i)
+    int temp;
+    answer.push_back(-1);
+    for(int i = 1; i < s.length(); i++)
     {
-        if (mp.find(s[i]) != mp.end()) answer.push_back(i - mp[s[i]]);
-        else answer.push_back(-1);
-        mp[s[i]] = i;
+        temp = -1;
+        for(int j = 0; j < i; j++)
+        {
+            if(s[j] == s[i])
+            {
+                temp = i - j;
+            }
+        }
+        answer.push_back(temp);
     }
+    
     return answer;
 }
