762c97f4d8e8952fcb2303b477ef1d4a32d6a2db
JakJang <jjgk24211@gmail.com>
2025-06-17 19:01:42 +0900

    [level 1] Title: 모의고사, Time: 0.32 ms, Memory: 3.86 MB -BaekjoonHub

---
+++ 프로그래머스/1/42840. 모의고사/모의고사.cpp
@@ -0,0 +1,41 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<int> first = {1, 2, 3, 4, 5};
+vector<int> second = {2, 1, 2, 3, 2, 4, 2, 5};
+vector<int> third = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+
+vector<int> solution(vector<int> answers) 
+{
+    vector<int> answer;
+    vector<int> count(3); // 크기가 3인 배열을 (default) 0으로 초기화
+    
+    for(int i = 0; i < answers.size(); i++)
+    {
+        if(answers[i] == first[i % first.size()])
+        {
+            count[0]++;
+        }
+        if(answers[i] == second[i % second.size()])
+        {
+            count[1]++;
+        }
+        if(answers[i] == third[i % third.size()])
+        {
+            count[2]++;
+        }
+    }
+    int score = *max_element(count.begin(), count.end());
+    
+    for(int i = 0; i < count.size(); i++)
+    {
+        if(count[i] == score)
+        {
+            answer.push_back(i+1);
+        }
+    }
+    
+    return answer;
+}
