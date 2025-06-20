2362faa1cc978b413b75916cc0c80f5525877b20
JakJang <jjgk24211@gmail.com>
2025-06-20 09:24:07 +0900

    [level 1] Title: 2016년, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.cpp
+++ 프로그래머스/1/12901. 2016년/2016년.cpp
@@ -5,7 +5,6 @@ using namespace std;
 
 string solution(int a, int b) 
 {
-    string answer = "";
     vector<int> month = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}; // 12월은 필요 없음
     vector<string> day = {"SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"};
     
@@ -16,8 +15,6 @@ string solution(int a, int b)
             b += month[i];
         }
     }
-    
-    answer = day[(b + 4) % 7]; // b에서 뺄 경우, 1월 6일까지 오류
-    
-    return answer;
+       
+    return day[(b + 4) % 7]; // b에서 뺄 경우, 1월 6일까지 오류
 }
