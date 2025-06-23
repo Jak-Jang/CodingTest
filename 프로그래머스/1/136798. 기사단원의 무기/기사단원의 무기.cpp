ee2191bf933f4929e257b48e631d391c57b73e08
JakJang <jjgk24211@gmail.com>
2025-06-23 16:38:28 +0900

    [level 1] Title: 기사단원의 무기, Time: 190.06 ms, Memory: 3.67 MB -BaekjoonHub

---
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.cpp
@@ -0,0 +1,39 @@
+#include <cmath>
+using namespace std;
+
+int Divisor(int num)
+{
+    int count = 0;
+    for(int i = 1; i <= sqrt(num); i++)
+    {
+        if(num % i == 0)
+        {
+            count += 2;
+        }
+        
+        if(i * i == num)
+        {
+            count--;
+        }
+    }
+    return count;
+}
+
+int solution(int number, int limit, int power) 
+{
+    int answer = 0;
+    
+    for(int i = 1; i <= number; i++)
+    {
+        if(Divisor(i) > limit)
+        {
+            answer += power;
+        }
+        else
+        {
+            answer += Divisor(i);
+        }
+    }
+    
+    return answer;
+}
