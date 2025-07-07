43dbffc684ef6255d889edd057032654abbf8f34
JakJang <jjgk24211@gmail.com>
2025-07-07 10:12:57 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.05 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.cpp
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.cpp
@@ -1,17 +1,13 @@
 #include <string>
 #include <vector>
-#include <algorithm> // 내가 이거 풀려고 find 배워왔다...
 
 using namespace std;
 
 string solution(vector<string> seoul) 
 {
-    string answer = "";
-    
-    auto it = find(seoul.begin(), seoul.end(), "Kim");
-    
-    int index = it - seoul.begin(); // 주소값- 첫번째 주소값
-
-    answer = "김서방은 " + to_string(index) + "에 있다";
-    return answer;
+    for(int i = 0; i < seoul.size(); i++)
+    {
+        if(seoul[i] == "Kim")
+            return "김서방은 " + to_string(i) + "에 있다";
+    }
 }
