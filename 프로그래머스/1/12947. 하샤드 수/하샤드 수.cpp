97e7263b6429f5645d09da7664baab92d6fc9ae4
JakJang <jjgk24211@gmail.com>
2025-07-10 10:11:13 +0900

    [level 1] Title: 하샤드 수, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.cpp
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.cpp
@@ -3,26 +3,16 @@ using namespace std;
 
 bool solution(int x) 
 {
-    bool answer = true;
     vector<int> vec;
-    int a = x;
-    int b = 0;
+    int a = x, b = 0;
     
-    for(int i = 0; a != 0; i++)
-    {
+    while(a != 0) {
         vec.push_back(a % 10);
-        a /= 10;       
+        a /= 10;
     }
     
-    for(int j = 0; j < vec.size(); j++)
-    {
-        b += vec[j];
-    }
+    for (int temp : vec) { b += temp; }
     
-    if(x % b != 0)
-    {
-        answer = false;
-    }
-    
-    return answer;
+    if(x % b != 0) { return false; }
+    else { return true; }
 }
