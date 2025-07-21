d6475fcc4e0ffc837226779f139137ebfbcd974f
JakJang <jjgk24211@gmail.com>
2025-07-21 09:40:13 +0900

    [level 2] Title: 의상, Time: 0.02 ms, Memory: 3.63 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.cpp
+++ 프로그래머스/2/42578. 의상/의상.cpp
@@ -9,6 +9,6 @@ int solution(vector<vector<string>> clothes)
     unordered_map<string, int> cases;
     int n = 1;
     for(const auto& it : clothes){ cases[it[1]]++; }
-    for(const auto& it : cases){ n *= it.second + 1; }
+    for(const auto& it : cases){ n *= (it.second + 1); }
     return n - 1;
 }
