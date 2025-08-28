75218e986451818c64e97f197cec8b40a2407c4f
JakJang <jjgk24211@gmail.com>
2025-08-28 10:41:24 +0900

    [level 1] Title: 모의고사, Time: 0.11 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.cpp
+++ 프로그래머스/1/42840. 모의고사/모의고사.cpp
@@ -1,27 +1,28 @@
 #include <vector>
-using namespace std;
 
-vector<int> first = {1, 2, 3, 4, 5};
-vector<int> second = {2, 1, 2, 3, 2, 4, 2, 5};
-vector<int> third = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+using namespace std;
 
 vector<int> solution(vector<int> answers) 
 {
+    vector<int> v1 = {1, 2, 3, 4, 5};
+    vector<int> v2 = {2, 1, 2, 3, 2, 4, 2, 5};
+    vector<int> v3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+    
     vector<int> answer;
     vector<int> count(3, 0);
     int maxScore = 0;
     
     for(int i = 0; i < answers.size(); i++)
     {
-        if(answers[i] == first[i % first.size()])
+        if(answers[i] == v1[i % v1.size()])
         {
             count[0]++;
         }
-        if(answers[i] == second[i % second.size()])
+        if(answers[i] == v2[i % v2.size()])
         {
             count[1]++;
         }
-        if(answers[i] == third[i % third.size()])
+        if(answers[i] == v3[i % v3.size()])
         {
             count[2]++;
         }
