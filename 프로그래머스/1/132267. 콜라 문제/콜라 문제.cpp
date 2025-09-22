c44ee4803d5932edec8108d7fc47d5f39c268d17
장재근 <jjgk24211@gmail.com>
2025-09-22 10:09:34 +0900

    [level 1] Title: 콜라 문제, Time: 0.03 ms, Memory: 3.64 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.cpp
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.cpp
@@ -2,11 +2,15 @@ using namespace std;
 
 int solution(int a, int b, int n) 
 {
-    int answer = 0, Return;
-    while(n >= a) {
+    int answer = 0;
+    int Return = 0;
+    
+    while(n >= a)
+    {
         Return = n / a;
         n = n - Return * a + Return * b;
         answer += Return * b;
     }
+    
     return answer;
 }
